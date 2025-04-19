a00345b12446462154f0a35636035626a95a3cae
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-19 23:35:53 +0800

    Celsius to Fahrenheit

---
+++ one-liners/1_convert_celsius_to_fahrenheit.js
@@ -0,0 +1,5 @@
+// celsiusToFahrenheit allows you to convert a temperature from Celsius to Fahrenheit. It takes a value in Celsius as
+// input and uses the formula (Celsius * 9/5) + 31 to perorm the conversion.exadecimal representation.
+
+const celsiusToFahrenheit = (celsius) => (celsius * 9/5) + 32;
+celsiusToFahrenheit(25); // 77
