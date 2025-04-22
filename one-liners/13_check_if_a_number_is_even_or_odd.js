239e07705e58ff0a9e37ca3cca1620f965874487
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-22 23:44:20 +0800

    Odd or Even number

---
+++ one-liners/13_check_if_a_number_is_even_or_odd.js
@@ -0,0 +1,7 @@
+// A simple JavaScript function named "isEven" determines if a number is even or odd. It takes a "num"
+// as input and returns true if even, false if odd. 
+
+const isEven = num => num % 2 === 0;
+
+console.log(isEven(2)); // true
+console.log(isEven(33)); // false
