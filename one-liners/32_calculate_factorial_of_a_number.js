9618e63fabbe60ab8d48e34c19574a070521a9a7
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-25 10:00:11 +0800

    calculate factorial of a number

---
+++ one-liners/32_calculate_factorial_of_a_number.js
@@ -0,0 +1,8 @@
+// Calculate the factorial of a given number.
+
+const factorial = (num) => {
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5)); // 120
