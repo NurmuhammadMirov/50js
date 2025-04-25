aad98991441f1b734b6b7ddde00f716f9d6f2d0d
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-25 10:24:07 +0800

    get current date in DDMMYYYY format

---
+++ one-liners/35_get_the_current_date_in_DDMMYYYY_format.js
@@ -0,0 +1,12 @@
+// Get the current date in the 'DD/MM/YYYY' format.
+
+const getCurrentDate = () => {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+console.log(getCurrentDate());
