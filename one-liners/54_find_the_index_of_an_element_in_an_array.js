cf5ca951392b477f147bd52a2bffa31701dbad1b
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-27 20:05:28 +0800

    find the index of an element in an array

---
+++ one-liners/54_find_the_index_of_an_element_in_an_array.js
@@ -0,0 +1,5 @@
+// find the index of a specific element in a given array.
+const findIndex = (arr, element) => arr.indexOf(element);
+
+const fruits = ['apple', 'banana', 'orange', 'grape'];
+console.log(findIndex(fruits, 'orange')); // 2 
