944ece633f62c5fe1d80cd2d5e4570095de4bbe9
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-04-28 14:22:14 +0800

    calculate the exponentiation of a number

---
+++ one-liners/59_calculate_the_exponentiation_of_a_number.js
@@ -0,0 +1,4 @@
+// Calculate the exponentiation of a given base raised to a specified exponent.
+const exponentiate = (base, exponent) => Math.pow(base, exponent);
+console.log(exponentiate(2, 3)); // 8
+console.log(exponentiate(5, 2)); // 25
