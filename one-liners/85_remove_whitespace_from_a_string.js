54d68f0b59ccb2ed9926758c5ecf13cc17bc78a6
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-05 12:12:26 +0800

    remove whitespace from a string

---
+++ one-liners/85_remove_whitespace_from_a_string.js
@@ -0,0 +1,3 @@
+// remove all whitespace character from a given string.
+const removeWhitespace = (str) => str.replace(/\s/g,'');
+console.log(removeWhitespace(" Hello, world! ")); // 'Hello,world!'
