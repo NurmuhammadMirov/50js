6dc93fe5c7ba2dd0f0bd985d6e32d0f028fece71
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-08 14:30:36 +0800

    find the median of an array of numbers

---
+++ one-liners/101_find_the_median_of_an_array_of_numbers.js
@@ -0,0 +1,8 @@
+// find the median of an array of numbers.
+const median = (arr) => {
+  const sorted = arr.sort((a, b) => a - b);
+  const mid = Math.floor(sorted.length / 2);
+  return sorted.length % 2=== 0 ? (sorted[mid - 1] + sorted[mid]) / 2 : sorted[mid];
+};
+
+console.log(median([1,3,5,7,9]));
