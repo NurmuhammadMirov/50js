1124d1b22c3af172cb6f5d92592a34bc6320af44
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-14 17:16:04 +0800

    calculate the perimeter of a rectangle

---
+++ one-liners/119_calculate_the_perimeter_of_a_rectangle.js
@@ -0,0 +1,4 @@
+// the rectanglePerimeter function calculates the perimeter of a rectangle by summing twice
+// the width and twice the height of the rectangle.
+const rectanglePerimeter = (width, height) => 2 * (width + height);
+console.log(rectanglePerimeter(5, 10)); // 30
