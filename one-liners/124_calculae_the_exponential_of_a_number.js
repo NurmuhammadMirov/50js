17bc60939023935bc4b8915ac525a747f36a8f84
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-16 13:19:13 +0800

    calculate the exponential of anumber

---
+++ one-liners/124_calculae_the_exponential_of_a_number.js
@@ -0,0 +1,5 @@
+// the exponential function calculates the result of raising a given base to a specified exponent 
+// using the exponentiation operator(**).
+
+const exponential = (base, exponent) => base ** exponent;
+console.log(exponential(2, 3)); // 8
