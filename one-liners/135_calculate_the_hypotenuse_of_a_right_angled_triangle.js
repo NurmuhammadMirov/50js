526098ebde349cc0ca01873ec08bc0aebc7ed7b1
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-19 17:03:22 +0800

    calculate the hypotenuse of a right angled triangle

---
+++ one-liners/135_calculate_the_hypotenuse_of_a_right_angled_triangle.js
@@ -0,0 +1,4 @@
+// the binaryToDecimalWithParseInt function converts a binary number to a decimal number using the
+// built-in parseInt function with the base parameter set to 2.
+const binaryToDecimalWithParseInt = (binary) => parseInt(binary, 2);
+console.log(binaryToDecimalWithParseInt("1101")); // 13
