238c1dac0510a434e4051abf80aaf5764664633b
Nurmuhammad Mirov <mirov0727@gmail.com>
2025-05-19 16:59:20 +0800

    reverse the order of words in a sentence

---
+++ one-liners/134_reverse_the_order_of_words_in_a_sentence.js
@@ -0,0 +1,3 @@
+// the reverseSentence function reverses the order of words in a given sentence.
+const reverseSentence = (sentence) => sentence.split('').reverse().join('');
+console.log(reverseSentence("Hello, how are you doing?"));
