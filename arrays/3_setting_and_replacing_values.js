26aa8c5b16655528e73b4870cc4ef13a675f8f68
Nurmuhammad Mirov <mirov0727@gmail.com>
2026-01-27 20:03:48 +0500

    setting/replacing values

---
+++ arrays/3_setting_and_replacing_values.js
@@ -0,0 +1,7 @@
+// ---setting/replacing values
+let greetings = ["hi", "sup", "hello", "yo", "hey"];
+greetings[2] = "hola";
+
+console.log(greetings);
+
+// at index position 2, we overwrite the initial 'hello' value with 'hola'
